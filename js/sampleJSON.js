bb70a9dd25c5d20a0fbc83934456f7c38e97d9f0
toyoppp <kentanumber2@gmail.com>
2024-07-28 10:34:35 +0900

    Add files via upload

---
+++ js/sampleJSON.js
@@ -0,0 +1,205 @@
+const sampleData = {
+  resultCount: 10,
+  results: [
+    {
+      wrapperType: "track",
+      kind: "song",
+      artistId: 1490256993,
+      collectionId: 1490256978,
+      trackId: 1490256995,
+      artistName: "YOASOBI",
+      collectionName: "夜に駆ける - Single",
+      trackName: "夜に駆ける",
+      collectionCensoredName: "夜に駆ける - Single",
+      trackCensoredName: "夜に駆ける",
+      artistViewUrl:
+        "https://music.apple.com/jp/artist/yoasobi/1490256993?uo=4",
+      collectionViewUrl:
+        "https://music.apple.com/jp/album/%E5%A4%9C%E3%81%AB%E9%A7%86%E3%81%91%E3%82%8B/1490256978?i=1490256995&uo=4",
+      trackViewUrl:
+        "https://music.apple.com/jp/album/%E5%A4%9C%E3%81%AB%E9%A7%86%E3%81%91%E3%82%8B/1490256978?i=1490256995&uo=4",
+      previewUrl:
+        "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview126/v4/78/0f/7f/780f7f48-d8f7-afcc-0cd1-7dc1bd7ee044/mzaf_1302732050487874408.plus.aac.p.m4a",
+      artworkUrl30:
+        "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/fe/58/79/fe58795c-1628-230a-9ffe-300c04b6627c/194491717186.jpg/30x30bb.jpg",
+      artworkUrl60:
+        "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/fe/58/79/fe58795c-1628-230a-9ffe-300c04b6627c/194491717186.jpg/60x60bb.jpg",
+      artworkUrl100:
+        "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/fe/58/79/fe58795c-1628-230a-9ffe-300c04b6627c/194491717186.jpg/100x100bb.jpg",
+      collectionPrice: 255,
+      trackPrice: 255,
+      releaseDate: "2019-12-15T12:00:00Z",
+      collectionExplicitness: "notExplicit",
+      trackExplicitness: "notExplicit",
+      discCount: 1,
+      discNumber: 1,
+      trackCount: 1,
+      trackNumber: 1,
+      trackTimeMillis: 261013,
+      country: "JPN",
+      currency: "JPY",
+      primaryGenreName: "J-Pop",
+      isStreamable: true,
+    },
+    {
+      wrapperType: "track",
+      kind: "song",
+      artistId: 1490256993,
+      collectionId: 1679278166,
+      trackId: 1679278167,
+      artistName: "YOASOBI",
+      collectionName: "アイドル - Single",
+      trackName: "アイドル",
+      collectionCensoredName: "アイドル - Single",
+      trackCensoredName: "アイドル",
+      artistViewUrl:
+        "https://music.apple.com/jp/artist/yoasobi/1490256993?uo=4",
+      collectionViewUrl:
+        "https://music.apple.com/jp/album/%E3%82%A2%E3%82%A4%E3%83%89%E3%83%AB/1679278166?i=1679278167&uo=4",
+      trackViewUrl:
+        "https://music.apple.com/jp/album/%E3%82%A2%E3%82%A4%E3%83%89%E3%83%AB/1679278166?i=1679278167&uo=4",
+      previewUrl:
+        "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview116/v4/6f/f5/eb/6ff5eb92-c1b9-1f31-3630-fe11bea18740/mzaf_16244429206474773401.plus.aac.p.m4a",
+      artworkUrl30:
+        "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/3a/17/eb/3a17eb30-eacb-82eb-51df-d1321dbb55bc/197188492205.jpg/30x30bb.jpg",
+      artworkUrl60:
+        "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/3a/17/eb/3a17eb30-eacb-82eb-51df-d1321dbb55bc/197188492205.jpg/60x60bb.jpg",
+      artworkUrl100:
+        "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/3a/17/eb/3a17eb30-eacb-82eb-51df-d1321dbb55bc/197188492205.jpg/100x100bb.jpg",
+      collectionPrice: 255,
+      trackPrice: 255,
+      releaseDate: "2023-04-12T12:00:00Z",
+      collectionExplicitness: "notExplicit",
+      trackExplicitness: "notExplicit",
+      discCount: 1,
+      discNumber: 1,
+      trackCount: 1,
+      trackNumber: 1,
+      trackTimeMillis: 213234,
+      country: "JPN",
+      currency: "JPY",
+      primaryGenreName: "J-Pop",
+      isStreamable: true,
+    },
+    {
+      wrapperType: "track",
+      kind: "song",
+      artistId: 1490256993,
+      collectionId: 1542182291,
+      trackId: 1542182540,
+      artistName: "YOASOBI",
+      collectionName: "THE BOOK",
+      trackName: "ハルジオン",
+      collectionCensoredName: "THE BOOK",
+      trackCensoredName: "ハルジオン",
+      artistViewUrl:
+        "https://music.apple.com/jp/artist/yoasobi/1490256993?uo=4",
+      collectionViewUrl:
+        "https://music.apple.com/jp/album/%E3%83%8F%E3%83%AB%E3%82%B8%E3%82%AA%E3%83%B3/1542182291?i=1542182540&uo=4",
+      trackViewUrl:
+        "https://music.apple.com/jp/album/%E3%83%8F%E3%83%AB%E3%82%B8%E3%82%AA%E3%83%B3/1542182291?i=1542182540&uo=4",
+      previewUrl:
+        "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/f2/09/ea/f209ea6c-eaa3-1ad8-c6e3-efbd66ee8c72/mzaf_3188029878656765318.plus.aac.p.m4a",
+      artworkUrl30:
+        "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/4b/ba/5b/4bba5b81-c56e-1513-4326-e24daf4a7bff/195497666737.jpg/30x30bb.jpg",
+      artworkUrl60:
+        "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/4b/ba/5b/4bba5b81-c56e-1513-4326-e24daf4a7bff/195497666737.jpg/60x60bb.jpg",
+      artworkUrl100:
+        "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/4b/ba/5b/4bba5b81-c56e-1513-4326-e24daf4a7bff/195497666737.jpg/100x100bb.jpg",
+      collectionPrice: 2037,
+      trackPrice: 255,
+      releaseDate: "2020-05-11T12:00:00Z",
+      collectionExplicitness: "notExplicit",
+      trackExplicitness: "notExplicit",
+      discCount: 1,
+      discNumber: 1,
+      trackCount: 9,
+      trackNumber: 3,
+      trackTimeMillis: 198600,
+      country: "JPN",
+      currency: "JPY",
+      primaryGenreName: "J-Pop",
+      isStreamable: true,
+    },
+    {
+      wrapperType: "track",
+      kind: "song",
+      artistId: 1490256993,
+      collectionId: 1542182291,
+      trackId: 1542182772,
+      artistName: "YOASOBI",
+      collectionName: "THE BOOK",
+      trackName: "ハルカ",
+      collectionCensoredName: "THE BOOK",
+      trackCensoredName: "ハルカ",
+      artistViewUrl:
+        "https://music.apple.com/jp/artist/yoasobi/1490256993?uo=4",
+      collectionViewUrl:
+        "https://music.apple.com/jp/album/%E3%83%8F%E3%83%AB%E3%82%AB/1542182291?i=1542182772&uo=4",
+      trackViewUrl:
+        "https://music.apple.com/jp/album/%E3%83%8F%E3%83%AB%E3%82%AB/1542182291?i=1542182772&uo=4",
+      previewUrl:
+        "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview115/v4/f3/82/c3/f382c352-d7a2-5847-5bd8-9f2d5973a98e/mzaf_3097056711403685485.plus.aac.p.m4a",
+      artworkUrl30:
+        "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/4b/ba/5b/4bba5b81-c56e-1513-4326-e24daf4a7bff/195497666737.jpg/30x30bb.jpg",
+      artworkUrl60:
+        "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/4b/ba/5b/4bba5b81-c56e-1513-4326-e24daf4a7bff/195497666737.jpg/60x60bb.jpg",
+      artworkUrl100:
+        "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/4b/ba/5b/4bba5b81-c56e-1513-4326-e24daf4a7bff/195497666737.jpg/100x100bb.jpg",
+      collectionPrice: 2037,
+      trackPrice: 255,
+      releaseDate: "2020-12-18T08:00:00Z",
+      collectionExplicitness: "notExplicit",
+      trackExplicitness: "notExplicit",
+      discCount: 1,
+      discNumber: 1,
+      trackCount: 9,
+      trackNumber: 7,
+      trackTimeMillis: 243613,
+      country: "JPN",
+      currency: "JPY",
+      primaryGenreName: "J-Pop",
+      isStreamable: true,
+    },
+    {
+      wrapperType: "track",
+      kind: "song",
+      artistId: 1490256993,
+      collectionId: 1528119626,
+      trackId: 1528119630,
+      artistName: "YOASOBI",
+      collectionName: "群青 - Single",
+      trackName: "群青",
+      collectionCensoredName: "群青 - Single",
+      trackCensoredName: "群青",
+      artistViewUrl:
+        "https://music.apple.com/jp/artist/yoasobi/1490256993?uo=4",
+      collectionViewUrl:
+        "https://music.apple.com/jp/album/%E7%BE%A4%E9%9D%92/1528119626?i=1528119630&uo=4",
+      trackViewUrl:
+        "https://music.apple.com/jp/album/%E7%BE%A4%E9%9D%92/1528119626?i=1528119630&uo=4",
+      previewUrl:
+        "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/bb/2d/a7/bb2da794-54ce-7e19-c04d-b1d0eff1947e/mzaf_15626598362023857192.plus.aac.p.m4a",
+      artworkUrl30:
+        "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/ae/7e/4a/ae7e4a28-fd46-9617-1066-fcbd124303d6/195497105656.jpg/30x30bb.jpg",
+      artworkUrl60:
+        "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/ae/7e/4a/ae7e4a28-fd46-9617-1066-fcbd124303d6/195497105656.jpg/60x60bb.jpg",
+      artworkUrl100:
+        "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/ae/7e/4a/ae7e4a28-fd46-9617-1066-fcbd124303d6/195497105656.jpg/100x100bb.jpg",
+      collectionPrice: 255,
+      trackPrice: 255,
+      releaseDate: "2020-09-01T12:00:00Z",
+      collectionExplicitness: "notExplicit",
+      trackExplicitness: "notExplicit",
+      discCount: 1,
+      discNumber: 1,
+      trackCount: 1,
+      trackNumber: 1,
+      trackTimeMillis: 248444,
+      country: "JPN",
+      currency: "JPY",
+      primaryGenreName: "J-Pop",
+      isStreamable: true,
+    },
+  ],
+};
